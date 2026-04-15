9faa8633198d68a73ec3a67c6c2d3d584b751380
TherLaf <therlaf0409@gmail.com>
2026-04-15 21:15:56 +0800

    Recreated file extensions data sets.

--- utils/data_sets.py
+++ utils/data_sets.py
@@ -1,16 +1,23 @@
+VIDE_EXTENSIONS = [
+    'webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gifv', 'mng',
+    'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm', 'asf', 'amv', 'mp4', 'm4p',
+    'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp',
+    '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod',
+]
+
+SUBTITLE_EXTENSIONS = ['srt']
+
+IMAGE_EXTENSIONS = [
+    ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".tif", ".webp",
+    ".svg", ".ico", ".heic", ".heif", ".raw", ".cr2", ".nef", ".arw",
+    ".psd", ".ai", ".eps"
+]
+
+
 def file_extensions() -> list:
     """Return a list of file extensions known to be encountered."""
     # Return a list of common video extensions.
-    return [
-        # Video extensions
-        'webm', 'mkv', 'flv', 'vob', 'ogv', 'ogg', 'rrc', 'gifv', 'mng',
-        'mov', 'avi', 'qt', 'wmv', 'yuv', 'rm', 'asf', 'amv', 'mp4', 'm4p',
-        'm4v', 'mpg', 'mp2', 'mpeg', 'mpe', 'mpv', 'm4v', 'svi', '3gp',
-        '3g2', 'mxf', 'roq', 'nsv', 'flv', 'f4v', 'f4p', 'f4a', 'f4b', 'mod',
-
-        # Subtitiles
-        'srt'
-    ]
+    return VIDE_EXTENSIONS + SUBTITLE_EXTENSIONS + IMAGE_EXTENSIONS
 
 
 def video_qualities() -> list:
